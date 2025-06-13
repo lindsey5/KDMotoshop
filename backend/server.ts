31b0028230e61e5056e61934a7283a9db7ec0ae7
lindsey5 <lindseysamson5@gmail.com>
2025-06-14 03:34:17 +0800

    initial commit

--- backend/server.ts
+++ backend/server.ts
@@ -39,7 +39,7 @@ const dirname = path.resolve();
 // Now you can use __dirname
 if (process.env.NODE_ENV === "production") { 
   app.use(express.static(path.join(dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.resolve(dirname, "frontend", "dist", "index.html"));
   });
 }
