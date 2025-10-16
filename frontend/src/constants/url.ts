ac88cf09f5f0aa11b7e58a7a38a49426b7f8fba7
lindsey5 <lindseysamson5@gmail.com>
2025-10-16 23:54:34 +0800

    commit

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,1 +1,1 @@
-export const url = process.env.NODE_ENV === 'production' ? 'https://kdmotoshop-ai-api.onrender.com/' : 'http://127.0.0.1:8000/';
+export const url = process.env.NODE_ENV === 'production' ? 'https://c41013b04f06.ngrok-free.app/' : 'http://127.0.0.1:8000/';
