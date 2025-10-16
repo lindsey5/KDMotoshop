20058f8d87b88bee41cdc78ba5a5d0b3dfd12c43
lindsey5 <lindseysamson5@gmail.com>
2025-10-17 00:35:33 +0800

    commit

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -1,1 +1,1 @@
-export const url = process.env.NODE_ENV === 'production' ? 'https://c41013b04f06.ngrok-free.app/' : 'http://127.0.0.1:8000/';
+export const url = process.env.NODE_ENV === 'production' ? 'https://bf612176672a.ngrok-free.app/' : 'http://127.0.0.1:8000/';
